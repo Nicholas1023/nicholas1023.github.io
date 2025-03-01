613f70cd295fbedfc6ec8cb3e566ef622b5f378f
Nicholas Lim <nicholas_lim@bbsshack.club>
2025-03-01 17:21:18 +0800

    Update keyboard shortcut

--- script.js
+++ script.js
@@ -5,22 +5,24 @@ function toggleDarkMode() {
 
   if (document.body.classList.contains("dark-mode")) {
     localStorage.setItem("mode", "dark");
-    toggleButton.textContent = "Light Mode (Ctrl+m)";
+    toggleButton.textContent = "Light Mode (Ctrl+Alt+M)";
   } else {
     localStorage.setItem("mode", "light");
-    toggleButton.textContent = "Dark Mode (Ctrl+m)";
+    toggleButton.textContent = "Dark Mode (Ctrl+Alt+M)";
   }
 }
 
 toggleButton.addEventListener("click", toggleDarkMode);
 
 document.addEventListener("keydown", (event) => {
-  if (event.ctrlKey && event.key.toLowerCase() === "m") {
+  if (event.ctrlKey && event.altKey && event.key.toLowerCase() === "m") {
+    event.preventDefault(); 
     toggleDarkMode();
   }
 });
 
+
 if (localStorage.getItem("mode") === "dark") {
   document.body.classList.add("dark-mode");
-  toggleButton.textContent = "Light Mode (Ctrl+m)";
+  toggleButton.textContent = "Light Mode (Ctrl+Alt+M)";
 }
