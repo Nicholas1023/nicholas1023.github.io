c33a7fbb17c5b72f10bc76c20da3941ebf9fbd7e
Nicholas Lim <nicholas_lim@bbsshack.club>
2025-07-08 20:31:00 +0800

    Add footer

--- script.js
+++ script.js
@@ -3,24 +3,25 @@ function search() {
     const content1 = document.getElementById("1");
     const content2 = document.getElementById("2");
     const content3 = document.getElementById("3");
+    const content4 = document.getElementById("4");
+    const content5 = document.getElementById("5");
+    const content6 = document.getElementById("6");
     const notFound = document.getElementById("notFound");
     notFound.style.display = "none";
+    reset(content1, content2, content3, content4, content5, content6)
     if (content1.textContent.toLowerCase().includes(term)) {
         content1.innerHTML = `<span class="result">${content1.innerHTML}</span>`;
-        reset(content2);
-        reset(content3);
     } else if (content2.textContent.toLowerCase().includes(term)) {
         content2.innerHTML = `<span class="result">${content2.innerHTML}</span>`;
-        reset(content1);
-        reset(content3);
     } else if (content3.textContent.toLowerCase().includes(term)) {
         content3.innerHTML = `<span class="result">${content3.innerHTML}</span>`;
-        reset(content1);
-        reset(content2);
+    } else if (content4.textContent.toLowerCase().includes(term)) {
+        content4.innerHTML = `<span class="result">${content4.innerHTML}</span>`;
+    } else if (content5.textContent.toLowerCase().includes(term)) {
+        content5.innerHTML = `<span class="result">${content5.innerHTML}</span>`;
+    } else if (content6.textContent.toLowerCase().includes(term)) {
+        content6.innerHTML = `<span class="result">${content6.innerHTML}</span>`;
     } else {
-        reset(content1);
-        reset(content2);
-        reset(content3);
         notFound.style.display = "block";
     };
 };
