4545fb0f2a7817593b62221e34ee85767e726c8d
Nicholas Lim <nicholas_lim@bbsshack.club>
2025-10-28 20:35:34 +0800

    Add downloads page

--- projects.js
+++ projects.js
@@ -5,7 +5,6 @@ function search() {
     const content3 = document.getElementById("3");
     const content4 = document.getElementById("4");
     const content5 = document.getElementById("5");
-    const content6 = document.getElementById("6");
     const notFound = document.getElementById("notFound");
     notFound.style.display = "none";
     reset(content1, content2, content3, content4, content5, content6)
@@ -19,8 +18,6 @@ function search() {
         content4.innerHTML = `<span class="result">${content4.innerHTML}</span>`;
     } else if (content5.textContent.toLowerCase().includes(term)) {
         content5.innerHTML = `<span class="result">${content5.innerHTML}</span>`;
-    } else if (content6.textContent.toLowerCase().includes(term)) {
-        content6.innerHTML = `<span class="result">${content6.innerHTML}</span>`;
     } else {
         notFound.style.display = "block";
     };
