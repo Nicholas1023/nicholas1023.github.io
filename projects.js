3e13a08f399361c8ff050ac92b1805cb854743ec
Nicholas Lim <nicholas_lim@bbsshack.club>
2026-04-15 21:10:29 +0800

    Update content

--- projects.js
+++ projects.js
@@ -5,9 +5,13 @@ function search() {
     const content3 = document.getElementById("3");
     const content4 = document.getElementById("4");
     const content5 = document.getElementById("5");
+    const content6 = document.getElementById("6");
+    const content7 = document.getElementById("7");
+    const content8 = document.getElementById("8");
+    const content9 = document.getElementById("9");
     const notFound = document.getElementById("notFound");
     notFound.style.display = "none";
-    reset(content1, content2, content3, content4, content5)
+    reset(content1, content2, content3, content4, content5, content6, content7)
     if (content1.textContent.toLowerCase().includes(term)) {
         content1.innerHTML = `<span class="result">${content1.innerHTML}</span>`;
     } else if (content2.textContent.toLowerCase().includes(term)) {
@@ -18,6 +22,14 @@ function search() {
         content4.innerHTML = `<span class="result">${content4.innerHTML}</span>`;
     } else if (content5.textContent.toLowerCase().includes(term)) {
         content5.innerHTML = `<span class="result">${content5.innerHTML}</span>`;
+    } else if (content6.textContent.toLowerCase().includes(term)) {
+        content6.innerHTML = `<span class="result">${content6.innerHTML}</span>`;
+    } else if (content7.textContent.toLowerCase().includes(term)) {
+        content7.innerHTML = `<span class="result">${content7.innerHTML}</span>`;
+    } else if (content8.textContent.toLowerCase().includes(term)) {
+        content8.innerHTML = `<span class="result">${content8.innerHTML}</span>`;
+    } else if (content9.textContent.toLowerCase().includes(term)) {
+        content9.innerHTML = `<span class="result">${content9.innerHTML}</span>`;
     } else {
         notFound.style.display = "block";
     };
