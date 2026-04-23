75ea057be5e6317ec187d269403fc3b018935cb4
Nicholas Lim <nicholas_lim@bbsshack.club>
2026-04-23 09:31:03 +0800

    Update script.js

--- terminal/script.js
+++ terminal/script.js
@@ -12,6 +12,8 @@ promptInput.addEventListener("keydown", function(e) {
             output.innerHTML = "Hello, welcome to my terminal web app!<br>help: Show this message.<br>projects: Lists my projects with a description."
         } else if (promptInput.value.toLowerCase() == "projects") {
             output.innerHTML = "1-bit Logic Gate Computer<br>This is my first PCB, A 1-bit computer that uses NOR and NOT gates made from 3<br>transistors as its processor, 3 Push buttons as input, and 2 LEDs as output.<br><br>Macro Microcontroller<br>A microcontroller and a macropad combined on a PCB which can be used for<br>controlling GPIO pins or running user programs instead of sending input to your<br>computer as a keyboard.<br><br>Nicholas' Code Editor<br>A simple HTML editor web app with an iframe renderer, open/save file,<br>boilerplate HTML code insertion and sharing function. Try it out at<br><a href='https://editor.nicholaslim.me'>https://editor.nicholaslim.me</a>!<br><br>Source Organiser<br>A user-friendly CLI file organiser for developers with more human-readable<br>commands. Project URL: <a href='https://pypi.org/project/source-organiser'>https://pypi.org/project/source-organiser</a>"
+        } else {
+            output.innerHTML = `"${promptInput.value.toLowerCase()}" is not a valid command. Enter "help" to get started!`;
         }
         promptContain = document.createElement("div");
         promptContain.innerHTML = `<p>&gt;&gt;&nbsp;</p><input type="text" id="prompt${count}"></input>`
